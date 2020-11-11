c9f34ddb1aeb8bf644e64fae4d650146b83fd514
Haner Avella <haneravella@MacBook-Pro-2.local>
2020-11-11 12:27:14 -0500

    Añade el archivo index.php y la vista NavBar

---
+++ views/navbar.php
@@ -0,0 +1,18 @@
+<!-- Barra de Navegación - Barra de Navegación -->
+<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+    <a class="navbar-brand" >
+        <img src="images/logoMecca.png" width="110" height="30" class="d-inline-block align-top mr-2" alt="" loading="lazy" />
+        TERPEL
+    </a>
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarNav">
+        <ul class="navbar-nav ml-auto">
+            <li class="nav-item active">
+                <a class="nav-link" href="index.php">Home <span class="sr-only">(current)</span></a>
+            </li>
+        </ul>
+    </div>
+</nav>
+<!-- Fin Barra de Navegación - Fin Barra de Navegación -->
