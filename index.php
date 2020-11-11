2c956db5e513cd0d36e15d9b0a97fc34bc101efb
JDaniel <daniel.jose.45@hotmail.com>
2020-11-11 15:41:24 -0500

    Añade pdf y pregunta 30

--- index.php
+++ index.php
@@ -1,14 +1,6 @@
-<?php
-// include the configs / constants for the database connection
-// require_once("config/db.php");
-// load the login class
-// require_once("classes/Login.php");
-// create a login object. when this object is created, it will do all login/logout stuff automatically
-// so this single line handles the entire login process. in consequence, you can simply ...
-// $login = new Login();
-?>
-
+<!--muestra_comercial_p10.php-->
 <!DOCTYPE html>
+
 <html lang="es">
 
 <head>
@@ -23,47 +15,8 @@
     <title>FERIA DEL HOGAR</title>
 </head>
 
-<body>
-    <!-- Barra de Navegación - Barra de Navegación -->
-    <?php
-    include("views/navbar.php")
-    ?>
-    <!-- Fin Barra de Navegación - Fin Barra de Navegación -->
+<body>    
+    <?php include("views/navbar.php")?>
+    <?php include("views/P30.php")?>
 
-    <div class="container">
-        <?php
-        include("views/cumplimiento_objetivos.php");
-        // include("views/pagina2.php");
-        ?>
-
-    </div>
-
-    <?php
-    // include("views/paginacion.php")
-    ?>
-    <!-- Optional JavaScript -->
-    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
-    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
-
-    <!-- <script>
-        var classList = document.getElementById('2').className.split(/\s+/);
-        for (var i = 0; i < classList.length; i++) {
-            if (classList[i] === 'active') {
-                console.log(`hola mundo`);
-                var x = document.getElementById("pag1");
-                // x.classList.add("oculto");
-                if (x.style.display === "none") {
-                    x.style.display = "block";
-                } else {
-                    x.style.display = "none";
-                }
-
-            }
-        }
-    </script> -->
-    <!-- <script src="js/index.js"></script> -->
-    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
-    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js" integrity="sha384-OgVRvuATP1z7JjHLkuOU7Xw704+h835Lr+6QL9UvYjZE3Ipu6Tp75j7Bh/kR0JKI" crossorigin="anonymous"></script>
 </body>
-
-<!-- </html> -->
