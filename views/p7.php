2068691d3bf72df4a87886db8ddfe16129462559
Haner Avella <haneravella@MacBook-Pro-2.local>
2020-11-11 16:31:00 -0500

    Finaliza visualizacion de la P7

---
+++ views/p7.php
@@ -0,0 +1,15 @@
+<div class="mt-5" id="p3">
+    <div class="col-xl-6 offset-xl-3 col-lg-6 offset-lg-3 col-md-8 offset-md-2">
+        <form method="post">
+            <div class="form-group">
+                <label for="P4">¿Por qué no realizaste compras?
+                </label>
+                <div class="md-form">
+                    <i class="fas fa-pencil-alt prefix"></i>
+                    <textarea id="form10" class="md-textarea form-control" rows="3"></textarea>
+                    <label for="form10">Icon Prefix</label>
+                </div>
+            </div>
+        </form>
+    </div>
+</div>
