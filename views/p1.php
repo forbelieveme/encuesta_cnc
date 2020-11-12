842107728e04f9dffd4fd80447dc3db020de2582
Haner Avella <haneravella@MacBook-Pro-2.local>
2020-11-11 19:43:40 -0500

    Inserta el metodo onsubmit() para el formulario

--- views/p1.php
+++ views/p1.php
@@ -1,11 +1,11 @@
 <div class="mt-5" id="p1">
     <div class="col-xl-6 offset-xl-3 col-lg-6 offset-lg-3 col-md-8 offset-md-2">
-        <form method="post">
+        <form method="post" id="form_p1" onsubmit="submit_by_id(); return false;">
             <div class="form-group">
                 <label for="P1">A continuación, le voy a mostrar una tarjeta con un listado
                     de frases de ese listado por favor me indica ¿Cuáles son las principales razones qué lo
                     motivaron a venir a la Feria del Hogar 2019?, ¿algún otro? </label>
-                <select multiple class="form-control .d-none .d-sm-block" id="exampleFormControlSelect2">
+                <select multiple class="form-control" id="exampleFormControlSelect2">
                     <option>Encontrar productos nuevos (lanzamientos)</option>
                     <option>Buscar promociones y descuentos</option>
                     <option>Por curiosidad</option>
@@ -17,9 +17,10 @@
                     <option>Es un plan diferente a ir a un Centro Comercial</option>
                 </select>
             </div>
-            <button type="submit" name="login" class="btn btn-primary btn-lg btn-block">
+            <button type="submit" id="btn_p1" class="btn btn-primary btn-lg btn-block">
                 INGRESAR
             </button>
         </form>
     </div>
 </div>
+
