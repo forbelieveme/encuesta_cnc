2d78c137ab68ee75ebee8c161f052cd69e56e018
Haner Avella <haneravella@MacBook-Pro-2.local>
2020-11-12 07:44:51 -0500

    Actualiza visualizacion de p18 y p19 para dar uniformidad con las vistas anteriores

--- views/P18.php
+++ views/P18.php
@@ -1,20 +1,22 @@
-<div class = "P18">
-
-    <div class = "jumbotron">
-        <div class = "container">
-            <div class = "row row-header">                
-                <div class = "col-sm-12">
-                    <h4>18. ¿Por qué razón calificas con (PONER RESPUESTA DE P17) la satisfacción general
-                    con los expositores de la Feria del Hogar 2020? </h4>
-                </div>                    
+<div class="mt-5" id="p3">
+    <div class="col-xl-6 offset-xl-3 col-lg-6 offset-lg-3 col-md-8 offset-md-2">
+        <form method='POST' onsubmit="getTextArea('P8_textArea', 'views/P19.php'); return false;">
+            <div class="form-group">
+                <div class="jumbotron">
+                    <div class="container">
+                        <div class="row row-header">
+                            <div class="col-sm-12">
+                                <label>18. ¿Por qué razón calificas con (PONER RESPUESTA DE P17) la satisfacción general
+                                    con los expositores de la Feria del Hogar 2020? </label>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <textarea id="P8_textArea" name="razon_calificacion" class="md-textarea form-control" rows="3"></textarea>
             </div>
-        </div>
-    </div>
-    <div class = "container">
-        <form method = 'POST' onsubmit="getTextArea('P8_textArea', 'views/p9.php'); return false;">
-            <textarea id ="P8_textArea" name="razon_calificacion" style="width:100%" rows = "10"></textarea>
-            <button style="margin-top: 10px;" type='submit'
-            class = "btn btn-primary btn-block"> Confirmar </button>    
+            <button type="submit" name="login" class="btn btn-primary btn-lg btn-block  mt-5">
+                SIGUIENTE
+            </button>
         </form>
     </div>
 </div>
