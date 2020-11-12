f7cea3c60719d68d45e02723c93ab6edc3635322
Haner Avella <haneravella@MacBook-Pro-2.local>
2020-11-11 19:44:09 -0500

    Añade metodo para insertar la primera vista en el index

--- index.php
+++ index.php
@@ -8,18 +8,15 @@
     <meta charset="utf-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
 
+    <script src="https://code.jquery.com/jquery-3.2.1.min.js"></script>
+
     <!-- Bootstrap CSS -->
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous" />
-    <link rel="stylesheet" href="css/style.css"> 
+    <link rel="stylesheet" href="css/style.css">
 
     <title>FERIA DEL HOGAR</title>
 </head>
 
-<<<<<<< HEAD
-<body>    
-    <?php include("views/navbar.php")?>
-    <?php include("views/P35.php")?>        
-=======
 <body>
     <!-- Barra de Navegación - Barra de Navegación -->
     <?php
@@ -29,11 +26,15 @@
 
     <div class="container">
         <?php
-        include("views/p16.php");
 
         // include("views/pagina2.php");
         ?>
+        <div id="pregunta">
 
+        </div>
+        <!-- <button id="btn" class="btn btn-primary btn-lg btn-block">
+            INGRESAR
+        </button> -->
     </div>
 
     <?php
@@ -41,10 +42,19 @@
     ?>
     <!-- Optional JavaScript -->
     <!-- jQuery first, then Popper.js, then Bootstrap JS -->
-    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+    <!-- <script src="https://code.jquery.com/jquery-3.2.1.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script> -->
     <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js" integrity="sha384-OgVRvuATP1z7JjHLkuOU7Xw704+h835Lr+6QL9UvYjZE3Ipu6Tp75j7Bh/kR0JKI" crossorigin="anonymous"></script>
+    <script src="js/main.js"></script>
+    <script>
+        $(document).ready(function() {
+            // $("#box").load("p1.html");
+            // $("#btn").click(function() {
+                $("#pregunta").load("views/p1.php");
+                //     $("#pregunta").load("views/p3.php");
+            // });
+        });
+    </script>
 </body>
->>>>>>> d1d5cd89b4ff338a6c698d297466167bcf6dfed6
 
 </body>
