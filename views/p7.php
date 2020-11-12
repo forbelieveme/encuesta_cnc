1cea2cef6ac3fd659604f33d11079f3b06be5413
Haner Avella <haneravella@MacBook-Pro-2.local>
2020-11-11 22:42:37 -0500

    Agrega metodo para obtener el contenido del text area y cambiar la pregunta

--- views/p7.php
+++ views/p7.php
@@ -1,13 +1,16 @@
 <div class="mt-5" id="p3">
     <div class="col-xl-6 offset-xl-3 col-lg-6 offset-lg-3 col-md-8 offset-md-2">
-        <form method="post">
+        <form method="post" onsubmit="getTextArea('P7_textArea', 'views/p8.php'); return false;">
             <div class="form-group">
                 <label for="P4">¿Por qué no realizaste compras?
                 </label>
                 <div class="md-form">
-                    <textarea id="form10" class="md-textarea form-control" rows="3"></textarea>
+                    <textarea id="P7_textArea" class="md-textarea form-control" rows="3"></textarea>
                 </div>
             </div>
+            <button type="submit" class="btn btn-primary btn-lg btn-block ">
+                SIGUIENTE
+            </button>
         </form>
     </div>
 </div>
