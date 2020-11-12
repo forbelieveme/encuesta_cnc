33d8098e979e0e975b711991e732ff440e4d910f
Haner Avella <haneravella@MacBook-Pro-2.local>
2020-11-11 21:53:14 -0500

    Agrega libreria para Select2

--- index.php
+++ index.php
@@ -7,8 +7,9 @@
     <!-- Required meta tags -->
     <meta charset="utf-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
-
+    <link href="https://cdn.jsdelivr.net/npm/select2@4.1.0-beta.1/dist/css/select2.min.css" rel="stylesheet" />
     <script src="https://code.jquery.com/jquery-3.2.1.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/select2@4.1.0-beta.1/dist/js/select2.min.js"></script>
 
     <!-- Bootstrap CSS -->
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous" />
@@ -50,8 +51,8 @@
         $(document).ready(function() {
             // $("#box").load("p1.html");
             // $("#btn").click(function() {
-                $("#pregunta").load("views/p1.php");
-                //     $("#pregunta").load("views/p3.php");
+            $("#pregunta").load("views/p1.php");
+            //     $("#pregunta").load("views/p3.php");
             // });
         });
     </script>
