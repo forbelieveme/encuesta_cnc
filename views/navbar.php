d6bc06c6956813a8245dd9ac31ed9fdff9393bde
Haner Avella <haneravella@MacBook-Pro-2.local>
2020-11-12 17:18:47 -0500

    Agrega el logo de la feria del hogar y actualiza el navBar

--- views/navbar.php
+++ views/navbar.php
@@ -1,18 +1,18 @@
 <!-- Barra de Navegación - Barra de Navegación -->
-<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+<nav class="navbar navbar-expand-lg ">
     <a class="navbar-brand" >
-        <img src="" width="110" height="30" class="d-inline-block align-top mr-2" alt="" loading="lazy" />
+        <img src="images/logo-feria.png" width="110" height="35" class="d-inline-block align-top mr-2" alt="" loading="lazy" />
         ENCUESTA
     </a>
-    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+    <!-- <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
-    </button>
-    <div class="collapse navbar-collapse" id="navbarNav">
+    </button> -->
+    <!-- <div class="collapse navbar-collapse" id="navbarNav">
         <ul class="navbar-nav ml-auto">
             <li class="nav-item active">
                 <a class="nav-link" href="index.php">Home <span class="sr-only">(current)</span></a>
             </li>
         </ul>
-    </div>
+    </div> -->
 </nav>
 <!-- Fin Barra de Navegación - Fin Barra de Navegación -->
