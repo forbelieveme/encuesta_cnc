62a1ac69360a3bfa257ea4364c9ca1f6e713b37a
Haner Avella <haneravella@MacBook-Pro-2.local>
2020-11-13 09:24:23 -0500

    Agrega la vista de finalizar

---
+++ views/finaliza.php
@@ -0,0 +1,15 @@
+<div class = "mt-5" id = "F3">
+    <div class="col-xl-6 offset-xl-3 col-lg-6 offset-lg-3 col-md-8 offset-md-2">
+
+            <div class = "jumbotron">
+                <div class = "container">
+                    <div class = "row row-header">                
+                        <div class = "col-sm-12">
+                            <label>¡TERMINASTE!</label>
+                        </div>                    
+                    </div>
+                </div>
+            </div>
+
+    </div>
+</div>
