70a0b339180032aeb180bd876153729621f13540
Haner Avella <haneravella@MacBook-Pro-2.local>
2020-11-13 19:48:47 -0500

    Añade metodo de submit a la pregunta p33

--- views/P33.php
+++ views/P33.php
@@ -1,6 +1,13 @@
+<script>
+    var numero_pregunta = 'p33'
+    var siguiente = 'views/P34.php';
+</script>
 <div class="mt-5" id="P33">
     <div class="col-xl-6 offset-xl-3 col-lg-6 offset-lg-3 col-md-8 offset-md-2">
-        <form method='POST'>
+        <form method='POST' onsubmit="
+        submit_decision(false, 1, 'textarea', numero_pregunta,
+        document.getElementById('taP34').value,  siguiente);       
+         return false;">
             <div class="jumbotron">
                 <div class="container">
                     <div class="row row-header">
@@ -12,7 +19,7 @@
             </div>
             <div class="form-group mt-5">
                 <div class="container">
-                    <textarea class="md-textarea form-control col-12 " id="P33" name="razon_calificacion" style="width:100%" rows="10"></textarea>
+                    <textarea class="md-textarea form-control col-12 " id="taP34" name="razon_calificacion" style="width:100%" rows="10"></textarea>
                     <button style="margin-top: 10px;" type='submit' class="btn btn-primary btn-block"> Confirmar </button>
                 </div>
             </div>
