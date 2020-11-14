79aa70d8585aeda47545fecc769af171d3490a48
Haner Avella <haneravella@MacBook-Pro-2.local>
2020-11-13 22:04:52 -0500

    Agrega metodo para recibir inofrmacion pero no funciona

--- views/P38.php
+++ views/P38.php
@@ -6,14 +6,13 @@
     <div class="col-xl-6 offset-xl-3 col-lg-6 offset-lg-3 col-md-8 offset-md-2">
         <form method='POST' onsubmit="
         submit_decision(false, 1, 'textarea', numero_pregunta,
-        document.getElementById('taP38').value,  siguiente);       
+        document.getElementById('taP38').value, siguiente, true);       
          return false;">
             <div class="jumbotron">
                 <div class="container">
                     <div class="row row-header">
                         <div class="col-sm-12">
-                            <label>38. ¿Por qué razón calificas con (PONER REPSUESTA DE P37) la satisfacción con la
-                                agenda de actividades de la Feria del Hogar 2020? </label>
+                            <label id="preg_p38"></label>
                         </div>
                     </div>
                 </div>
@@ -27,3 +26,8 @@
         </form>
     </div>
 </div>
+<script>
+    $(document).ready(function() {
+        cambia_texto_pregunta("preg_p38");
+    });
+</script>
