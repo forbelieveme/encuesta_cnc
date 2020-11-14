2ee9b9d75294f81716f85b183f8c39391f586377
JDaniel <daniel.jose.45@hotmail.com>
2020-11-14 02:48:26 -0500

    added D567 capturando datos

---
+++ views/D567.php
@@ -0,0 +1,43 @@
+<script>
+    var numero_pregunta = 'D567';
+    var siguiente = 'views/P43.php';
+</script>
+<div class = "mt-5" id = "P42">
+    <div class="col-xl-6 offset-xl-3 col-lg-6 offset-lg-3 col-md-8 offset-md-2">
+        <form method = 'POST' onsubmit="
+        submit_personales(
+        document.getElementById('nombre').value, 
+        document.getElementById('correo').value, 
+        document.getElementById('numero').value);
+        return false;
+        ">
+            <div class = "jumbotron">
+                <div class = "container">
+                    <div class = "row row-header">                
+                        <div class = "col-sm-12">
+                            <label>Datos Demográficos</label>
+                        </div>                    
+                    </div>
+                </div>
+            </div>
+
+            
+                <div class = "container mb-2">                    
+                    <label for ='nombre'>Nombre</label>
+                    <textarea class ="md-textarea form-control col-12 " id ="nombre" name="razon_calificacion" style = "widht:100%" rows="1" ></textarea>
+                    
+                </div>
+                <div class = "container mb-2">  
+                    <label for ='correo'>Correo Electrónico</label>                  
+                    <textarea class ="md-textarea form-control col-12 " id ="correo" name="razon_calificacion" style = "widht:100%" rows="1" ></textarea>
+                    
+                </div>
+                <div class = "container mb-2">                    
+                    <label for ='numero'>Número de Teléfono: </label>
+                    <textarea class ="md-textarea form-control col-12 " id ="numero" name="razon_calificacion" style = "widht:100%" rows="1" ></textarea>                    
+                </div>
+                <button type="submit" name="login" class="btn btn-primary btn-lg btn-block  mt-5"> SIGUIENTE </button>
+            
+        </form>
+    </div>
+</div>
