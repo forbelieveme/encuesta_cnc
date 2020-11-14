e957da25b43fcca298ba61180efb239b948e372d
Haner Avella <haneravella@MacBook-Pro-2.local>
2020-11-13 21:10:58 -0500

    Configura la p35 para multiple seleccion

--- views/P35.php
+++ views/P35.php
@@ -1,51 +1,42 @@
-<div class = "mt-5" id= "P35">
+<script>
+    $(".js-example-placeholder-multiple").select2({
+        theme: "classic",
+        placeholder: "Seleccione...",
+    });
+    $(function() {
+        $(".js-example-basic-multiple").select2();
+    });
+    var numero_pregunta = 'p1';
+    var siguiente = 'views/p2.php';
+</script>
+<div class="mt-5" id="p1">
     <div class="col-xl-6 offset-xl-3 col-lg-6 offset-lg-3 col-md-8 offset-md-2">
-        <form method = 'POST'>              
-                <div class = "jumbotron">
-                    <div class = "container">
-                        <div class = "row row-header">                
-                            <div class = "col-sm-12">
-                                <h4>35. Del listado de actividades de Feria 
-                                    del Hogar 2020 ¿En cuáles actividades participaste? </h4>
-                            </div>                    
-                        </div>
-                    </div>
+        <div class="jumbotron">
+            <label>
+                35. ¿Conociste o te enteraste sobre la agenda de actividades de la Feria del Hogar 2020?
+            </label>
+        </div>
+        <form method="post" id="form_p1" onsubmit="
+        var elementos = ObtenerChecked(document.getElementsByTagName('select')[0]);        
+        elementos.push(document.getElementById('form10').value);
+        submit_decision(false, 1, 'otrocual', numero_pregunta, elementos.join(','),
+        siguiente);
+        return false;" class="">
+            <div class="form-group">
+                <select multiple class="form-control js-example-basic-multiple js-example-placeholder-multiple" id="exampleFormControlSelect2">
+                    <option value="1">Charlas “Un café con expertos</option>
+                    <option value="2">Asesorías “Hazlo tú mismo” en el pabellón 6 y 17</option>
+                    <option value="3">Actividades y compras del Blackfriday (SÓLO APARECE EL 27 DE NOVIEMBRE)</option>
+                    <option value="4">Actividades Y compras en el trasnochón (SÓLO APARECE EL 4 DE DICIEMBRE)</option>
+                </select>
+                <div class="radioboton form-check">
+                    <input class='form-check-input' type='radio' name='opcionn' id='P27_6' value='6'>
+                    <label class='form-check-label' for='P27_6'>Ninguno de estos</label>
                 </div>
-
-                <div class="form-group mt-5">
-                    <div class = "container">
-                        <div class='form-check form-check-inline col-12'>                                                       
-                            <input type='checkbox' name='check' id='P35_1' value='1' checked>
-                            <label  class='form-check-label escala'  style = "margin-top: 5px" for='P35_1' >Shows tarimas artistas invitados  </label>
-                        </div>                         
-                    </div>
-                </div>
-
-                <div class="form-group mt-5">
-                    <div class = "container">
-                        <div class='form-check form-check-inline col-12'>                                                       
-                            <input  type='checkbox' name='check' id='P35_2' value='2'>
-                            <label   class='form-check-label escala' style = "margin-top: 5px" for='P35_2' >Charlas auditorio principal  </label>
-                        </div>  
-                    </div>
-                </div>
-
-                <div class="form-group mt-5">
-                    <div class = "container">
-                        <div class='form-check form-check-inline col-12'>                        
-                            <input  type='radio' name='opcion' id='P35_3' value='3'>
-                            <label   class='form-check-label escala' style = "margin-top: 5px"  for='P35_3' >Ninguna </label>
-                        </div>  
-                    </div>
-                </div>
-
-                <div class="form-group mt-5">
-                    <div class = "container">
-                    <button type="submit" name="login" class="btn btn-primary btn-lg btn-block  mt-5"> SIGUIENTE </button>
-                    </div>
-                </div>
-                    
-              
+            </div>
+            <button type="submit" id="btn_p1" class="btn btn-primary btn-lg btn-block mt-5" onclick="">
+                SIGUIENTE
+            </button>
         </form>
     </div>
 </div>
