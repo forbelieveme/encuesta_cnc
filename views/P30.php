3f9c94338f04df2f5d86c4fd46c2aef5fff4a0c3
Haner Avella <haneravella@MacBook-Pro-2.local>
2020-11-13 20:30:58 -0500

    Cambia el archivo con el contenido porque los radio botones no estaban funcionando

--- views/P30.php
+++ views/P30.php
@@ -1,33 +1,32 @@
-<div class = "mt-5" id= "P30">
+<script>
+    var numero_pregunta = 'p30';
+    var siguiente = ['views/P31.php', 'views/P34.php'];
+</script>
+<div class="mt-5" id="p30">
     <div class="col-xl-6 offset-xl-3 col-lg-6 offset-lg-3 col-md-8 offset-md-2">
-        <form method='POST'>
-            <div class = "jumbotron">
-                <div class = "container">
-                    <div class = "row row-header">                
-                        <div class = "col-sm-12">
-                            <label>30. ¿Recuerdas haber visto, leído o escuchado publicidad de la Feria del Hogar 2020?</label>
-                        </div>                    
+        <form method="post" id="form_p30" onsubmit="submit_decision(false, 1, 'radio', numero_pregunta, 
+         getRadioVal(document.getElementById('form_p30'),'P30'), 
+         Obtener_siguiente(numero_pregunta, getRadioVal(document.getElementById('form_p30'),'P30'),siguiente) );
+          return false;">
+            <div class="form-group">
+                <div class="jumbotron">
+                    <label for="P30">30. ¿Recuerdas haber visto, leído o escuchado publicidad de la Feria del Hogar 2020?
+                    </label>
+                </div>
+                <div class="container text-center">
+                    <div class="custom-control custom-radio custom-control-inline form-check-inline">
+                        <input type="radio" id="P30_1" value='1' name="P30" class="custom-control-input">
+                        <label class="custom-control-label" for="P30_1">Sí</label>
+                    </div>
+                    <div class="custom-control custom-radio custom-control-inline form-check-inline">
+                        <input type="radio" id="P30_2" value='2' name="P30" class="custom-control-input">
+                        <label class="custom-control-label" for="P30_2">No</label>
                     </div>
                 </div>
             </div>
-            
-            <div class = "form-group mt-5">
-                <div class = "container">
-                    <div class = "form-check col-12">
-                        <div class = 'radioboton'>                         
-                            <input  class='form-check-input' type='radio' name='opcion' id='P30_si' value='1' checked>
-                            <label  class='form-check-label escala' for = 'P30_si' >Sí  </label>
-                        </div>                         
-                        <div class = 'radioboton'>                         
-                            <input  class='form-check-input'  type='radio' name='opcion' id='P30_no' value='2'>
-                            <label  class='form-check-label escala' for = 'P30_no' >No  </label>
-                        </div>        
-                    </div>    
-                </div>    
-            </div>    
-            <div class = "form-group mt-5">                                          
-                <button type="submit" name="login" class=" col-12 btn btn-primary btn-lg btn-block  mt-5"> SIGUIENTE </button>
-            </div>
+            <button type="submit" name="login" class="btn btn-primary btn-lg btn-block">
+                SIGUIENTE
+            </button>
         </form>
-    </div>      
+    </div>
 </div>
