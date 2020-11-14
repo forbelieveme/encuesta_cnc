3d94fc001bec2a16cc92ba34a524ffe5c5d1f374
JDaniel <daniel.jose.45@hotmail.com>
2020-11-14 00:30:57 -0500

    punto 27 recoge datos

--- views/P27.php
+++ views/P27.php
@@ -1,10 +1,22 @@
 <script>
+    $(".js-example-placeholder-multiple").select2({
+        theme: "classic",
+        placeholder: "Seleccione...",
+    });
+    $(function() {
+        $(".js-example-basic-multiple").select2();
+    });
     var numero_pregunta = 'p27';
     var siguiente = 'views/P29.php';
 </script>
 <div class = "mt-5" id="P27">
     <div class="col-xl-6 offset-xl-3 col-lg-6 offset-lg-3 col-md-8 offset-md-2">
-        <form method = 'POST'>
+        <form method = 'POST' onsubmit = "
+            submit_parametro(
+            document.getElementById('ninguno').checked,
+            document.getElementsByTagName('select')[0]
+            );
+            return false;">
             <div class = "jumbotron">
                 <div class = "container">
                     <div class = "row row-header">                
@@ -14,44 +26,25 @@
                     </div>
                 </div>
             </div>
-            <div class = "form-group mt-5" >                
-                    <div class = "container">
-                        <div class = "radioboton form-check">                         
-                            <input class = 'form-check-input' type='checkbox' name='opcion' id='P27_1' value='1'>
-                            <label  class = "radioboton-label" for = 'P27_1' > Parqueadero Verde </label>
-                        </div>           
-
-
-                        <div class = "radioboton form-check">                         
-                            <input class = 'form-check-input' type='checkbox' name='opcion' id='P27_2' value='2' >
-                            <label class = 'form-check-label escala'  for = 'P27_2' > Parqueadero Torre</label>
-                        </div>           
-
-
-                        <div class = "radioboton form-check">                         
-                            <input class = 'form-check-input' type='checkbox' name='opcion' id='P27_3' value='3' >
-                            <label class = 'form-check-label escala'  for = 'P27_3' > Maletero / Paquetero </label>
-                        </div>           
-
-
-                        <div class = "radioboton form-check">                         
-                            <input class = 'form-check-input' type='checkbox' name='opcion' id='P27_4' value='4' >
-                            <label class = 'form-check-label escala'  for = 'P27_4' > Plazoleta de comidas </label>
-                        </div>    
-
-
-                        <div class = "radioboton form-check">                         
-                            <input class = 'form-check-input' type='checkbox' name='opcion' id='P27_5' value='5' >
-                            <label class = 'form-check-label escala'  for = 'P27_5' > Punto de información </label>
-                        </div>                                                         
-
-
-                        <div class = "radioboton form-check">                         
-                            <input class = 'form-check-input' type='radio' name='opcionn' id='P27_6' value='6' >
-                            <label class = 'form-check-label escala' for = 'P27_6' >Ninguno de estos</label>
-                        </div>    
-                    </div>  
-            </div>  
+            <div class="form-group mt-5" >
+                <select multiple id="select_p27" class="form-control js-example-basic-multiple js-example-placeholder-multiple">
+                    <option value="1">Parqueadero Verde</option>
+                    <option value="2">Parqueadero Torre</option>
+                    <option value="3">Maletero / Paquetero</option>
+                    <option value="4">Plazoleta de comidas</option>
+                    <option value="7">Punto de información </option>
+                    
+                </select>                
+                <div class = "radioboton form-check">                         
+                        <input class = 'form-check-input' type='checkbox' name='opcion' id='ninguno' 
+                        onclick="
+                        activar_desactivar(
+                        document.getElementById('ninguno').checked, 
+                        document.getElementsByTagName('select')[0])">
+                        <label  class = "radioboton-label" for = 'P27_1' > Ninguno de estos</label>
+                </div>   
+            </div>
+            
             
             <div class = "form-group mt-5"> 
                 <button style="margin-top: 10px;" type='submit'  class = "btn btn-primary btn-block"> Confirmar </button>    
