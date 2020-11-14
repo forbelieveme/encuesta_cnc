c61d41f0ace78f5dd6dd9ea278524fb1168dfb3e
Haner Avella <haneravella@MacBook-Pro-2.local>
2020-11-14 11:48:08 -0500

    Actualiza el formulario para usar custom radio buttons

--- views/p3.php
+++ views/p3.php
@@ -1,7 +1,6 @@
 <script>
     var numero_pregunta = "p3";
     siguiente = "views/p4.php"
-
 </script>
 <div class="mt-5" id="p3">
     <div class="col-xl-6 offset-xl-3 col-lg-6 offset-lg-3 col-md-8 offset-md-2">
@@ -9,7 +8,7 @@
         getRadioVal(document.getElementById('form_p3'),'P3'), siguiente, true);
         return false;">
             <div class="form-group">
-                <div class="jumbotron">
+                <div class="jumbotron mb-5">
                     <label for="P3">3. Teniendo en cuenta una escala de 1 a 10, donde 1 es “Muy insatisfecho” y
                         10 es “Muy satisfecho” ¿Cuál es tu nivel de satisfacción general con respecto al
                         cumplimiento de todos los motivos que lo llevaron a venir a la Feria del Hogar 2020?
@@ -18,10 +17,10 @@
                 <div class="container">
                     <?php
                     for ($counter = 1; $counter < 11; $counter++) {
-                        echo "<div class='form-check form-check-inline col-1'>";
-                        echo "<input class='form-check-input' type='radio' name='P3' id='P3_" . $counter . "' value='" . $counter . "'>";
-                        echo "<label class='form-check-label escala' for='inlineRadio3'>" . $counter . "</label>";
-                        echo "</div";
+                        echo "<div class='custom-control custom-radio custom-control-inline form-check-inline escala'>";
+                        echo "<input class='custom-control-input' type='radio' name='P3' id='P3_" . $counter . "' value='" . $counter . "'>";
+                        echo "<label class='custom-control-label escala' for='P3_" . $counter . "'>" . $counter . "</label>";
+                        echo "</div>";
                     }
                     ?>
                 </div>
